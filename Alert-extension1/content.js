1722f7de5cabc80c71a52b2dbcfe7674e93604b6
CodeNoor <96106257+CodeNoor@users.noreply.github.com>
2021-12-16 09:44:28 +0530

    Add files via upload

---
+++ Alert-extension1/content.js
@@ -0,0 +1,15 @@
+console.log('This is working......');
+// window.open("https://www.w3schools.com");
+// newtab = location.href;
+// console.log(newtab);
+
+// if (newtab == 'https://www.codewithharry.com/' || newtab == 'http://127.0.0.1:5500/program/tut1.html') {
+//     console.log('You are on code with harry and mindfullness');
+
+// }
+// // console.log(newtab);
+
+// else {
+//     console.log("My Salary equal to 1000");
+//     location.href = 'http://127.0.0.1:5500/program/tut1.html'
+// }
